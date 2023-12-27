30e737737b67983f515eef85a2839f1c15401511
Eduardo Humberto <eduardo.humberto1992@gmail.com>
2023-12-27 09:50:45 -0300

    first commit

---
+++ codegen/testserver/wrapped_type.go
@@ -0,0 +1,8 @@
+package testserver
+
+import "github.com/99designs/gqlgen/codegen/testserver/otherpkg"
+
+type WrappedScalar = otherpkg.Scalar
+type WrappedStruct otherpkg.Struct
+type WrappedMap otherpkg.Map
+type WrappedSlice otherpkg.Slice
